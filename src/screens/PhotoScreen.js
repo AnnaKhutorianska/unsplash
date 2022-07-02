1f1007730056941457ca125dd3965bf55f4ce459
Anna Khutorianska <annakhutorianska@gmail.com>
2022-07-03 00:01:42 +0300

    add button back

--- src/screens/PhotoScreen.js
+++ src/screens/PhotoScreen.js
@@ -1,40 +1,51 @@
 import React from 'react';
-import { ImageBackground,Text, Image, StyleSheet, Button } from 'react-native';
+import { ImageBackground, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 
+function PhotoScreen({navigation}) {
+	const photo = useSelector((state) => state.selectedPhoto?.urls?.full);
 
-function PhotoScreen() {
-	const photo = useSelector(state => state.selectedPhoto?.urls?.small);
+	function handlePress() {
+		navigation.goBack();
+	}
 
 	return (
-		<View style={styles.container}>
-			<ImageBackground
-			style={styles.image}
-			source={{ uri: photo }}
-		>
-			<Button style={styles.button} title='fsdfss'/>
+		<ImageBackground source={{ uri: photo }} style={styles.image}>
+			<View style={styles.wrapper}>
+				<TouchableOpacity onPress={handlePress} style={styles.appButtonContainer}>
+					<Text style={styles.appButtonText}>Go Back</Text>
+				</TouchableOpacity>
+			</View>
 		</ImageBackground>
-		</View>
-		
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-	  flex: 1,
-	//   position: 'relative'
-	},
 	image: {
-	//   flex: 1,
-	  position: 'relative'
-	//   justifyContent: 'center'
+		flex: 1,
+		resizeMode: 'contain'
 	},
-	button: {
-		position: 'absolute',
-		top: 20
-	}
-  });
+	wrapper: {
+		width: 100,
+		marginTop: 50,
+		marginLeft: 10
+	},
+	appButtonContainer: {
+		elevation: 7,
+		backgroundColor: '#fff',
+		borderRadius: 10,
+		paddingVertical: 8,
+
+	},
+	appButtonText: {
+		fontSize: 15,
+		color: '#000',
+		fontWeight: 'bold',
+		alignSelf: 'center',
+		textTransform: 'uppercase',
+	},
+});
 
 export default PhotoScreen;
